869a0192463a5da0df2dd848fff6bc2d89910830
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-25 12:59:20 +0530

    Add files via upload
    Pandas Series provide various attributes such as index, values, data type, and shape. These attributes help in understanding the structure, content, and type of data stored in the Series.

---
+++ PANDAS/seven.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+s = pd.Series([5, 10, 15])
+
+print(s.index)     # Index labels
+print(s.values)    # Data values
+print(s.dtype)     # Data type
+print(s.shape)     # Shape of series
